921a865079af690b1d871ff5a37c62c19d43565d
FemmeSTEMGem <femmestemgem@gmail.com>
2022-03-22 14:14:00 -0400

    Commented out session check for development testing

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,15 +5,15 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
+    # if user && user.authenticate(params[:password])
+    #   # Save the user id inside the browser cookie. This is how we keep the user 
+    #   # logged in when they navigate around our website.
       session[:user_id] = user.id
       redirect_to '/'
-    else
+    # else
     # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
-    end
+      # redirect_to '/login'
+    # end
   end
 
   def destroy
