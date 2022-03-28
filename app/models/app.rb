788a489b0f237422e1cea8788f9d82e9d22cbf9d
Joe Valookaran <joevalookaran@hotmail.com>
2022-03-28 15:14:21 +0000

    added pdf for resume

--- app/models/app.rb
+++ app/models/app.rb
@@ -3,6 +3,7 @@ class App < ApplicationRecord
   belongs_to :company
   belongs_to :user
   has_many :interviews
+  has_one_attached :resume
 
   validates_presence_of :job_title, :resume, :applied_date, :application_status
 
